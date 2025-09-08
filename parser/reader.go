bc9a62112db3229c7b19a7e46c8d43528ba76cf8
Vaastav Anand <vaastav.anand05@gmail.com>
2025-09-08 11:22:02 +0200

    Add simple file reader

--- parser/reader.go
+++ parser/reader.go
@@ -1,7 +1,56 @@
 package parser
 
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Source int
+
+type ProcessFn func(context.Context, string) error
+
+const (
+	LogFile Source = iota
+	NamedPipe
+)
+
 type Reader struct {
+	SrcType Source
+	Path    string
 }
 
-type LogLine struct {
+func NewReader(ctx context.Context, path string, src Source) (*Reader, error) {
+	return &Reader{SrcType: src, Path: path}, nil
+}
+
+func (r *Reader) ProcessLog(ctx context.Context, fn ProcessFn) error {
+	if r.SrcType == LogFile {
+		return r.ProcessFromLogFile(ctx, fn)
+	}
+	return errors.New(fmt.Sprintf("Unsupported source file type %d", int(r.SrcType)))
+}
+
+func (r *Reader) ProcessFromLogFile(ctx context.Context, fn ProcessFn) error {
+	file, err := os.Open(r.Path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Handle empty log lines
+		if line == "" {
+			continue
+		}
+		err := fn(ctx, line)
+		if err != nil {
+			// Early exit if we find an error during processing
+			return err
+		}
+	}
+	return nil
 }
