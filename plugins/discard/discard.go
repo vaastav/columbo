221a34c16df61e7fae0d1065a483336327a193b2
Vaastav Anand <vaastav.anand05@gmail.com>
2025-09-19 14:17:06 +0200

    Add discard plugin

---
+++ plugins/discard/discard.go
@@ -0,0 +1,37 @@
+package discard
+
+import (
+	"context"
+	"log"
+
+	"github.com/vaastav/columbo_go/components"
+	"github.com/vaastav/columbo_go/trace"
+)
+
+type DiscardSink struct {
+	InStream *components.DataStream
+	ID       int
+	counter  uint64
+}
+
+func NewDiscardSink(ctx context.Context, instream *components.DataStream, id int) (*DiscardSink, error) {
+	ds := &DiscardSink{
+		InStream: instream,
+		ID:       id,
+	}
+	return ds, nil
+}
+
+func (ds *DiscardSink) Run(ctx context.Context) error {
+	for v := range ds.InStream.Data {
+		ds.counter += 1
+		ds.doNothing(v)
+		if ds.counter%1000 == 0 {
+			log.Println("[DS-", ds.ID, "]Processed ", ds.counter, " incoming traces")
+		}
+	}
+
+	return nil
+}
+
+func (ds *DiscardSink) doNothing(t *trace.ColumboTrace) {}
