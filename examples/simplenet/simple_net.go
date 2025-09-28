f4089802592af2a365d75ef823f9ee6f4ca53b72
Vaastav Anand <vaastav.anand05@gmail.com>
2025-09-28 21:34:47 +0200

    Move columbo initialization to common folder

--- examples/simplenet/simple_net.go
+++ examples/simplenet/simple_net.go
@@ -2,58 +2,24 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 	"sync"
 
 	"github.com/vaastav/columbo_go/components"
 	"github.com/vaastav/columbo_go/plugins/discard"
 	"github.com/vaastav/columbo_go/symphony"
-	"github.com/vaastav/columbo_go/topology"
 )
 
 const (
 	BUFFER_SIZE = 65536
 )
 
-var topology_file = flag.String("topology", "", "Path to topology file")
-var logmapping = flag.String("log", "", "Path to log mapping file")
-
 func main() {
-	flag.Parse()
-	if *topology_file == "" {
-		log.Println("topology file for the simulation not provided")
-		log.Fatal("Usage: go run simple_net.go -topology=path/to/topology.json -log=path/to/log_mapping.json")
-	}
-
-	if *logmapping == "" {
-		log.Println("Log mapping file for the simulation not provided")
-		log.Fatal("Usage: go run simple_net.go -topology=path/to/topology.json -log=path/to/log_mapping.json")
-	}
-
-	topo, err := topology.ParseTopology(*topology_file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lm, err := topology.ParseLogMapping(*logmapping)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// Create readers and set up the simulator instances
-	readers, err := symphony.CreateReaders(ctx, lm)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sim_instances, err := symphony.CreateSimInstanceFromTopology(ctx, topo, BUFFER_SIZE)
-	if err != nil {
-		log.Fatal(err)
-	}
+	readers, sim_instances, err := symphony.InitializeFromFile(ctx, BUFFER_SIZE)
 
 	idx := 0
 
--- symphony/opgraph.go
+++ symphony/opgraph.go
@@ -2,13 +2,18 @@ package symphony
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"sync"
 
 	"github.com/vaastav/columbo_go/components"
 	"github.com/vaastav/columbo_go/parser"
+	"github.com/vaastav/columbo_go/topology"
 )
+
+var topology_file = flag.String("topology", "", "Path to topology file")
+var logmapping = flag.String("log", "", "Path to log mapping file")
 
 func StartProcessing(readers map[string]*parser.Reader, sim_instances map[string]*SimInstance) error {
 	ctx := context.Background()
@@ -39,4 +44,41 @@ func LauncOpGraph(ctx context.Context, wg *sync.WaitGroup, sinks []components.Pl
 	for _, sink := range sinks {
 		sink.Launch(ctx, wg)
 	}
+}
+
+func Initialize(ctx context.Context, lm *topology.LogMapping, topo *topology.Topology, buffer_size int) (map[string]*parser.Reader, map[string]*SimInstance, error) {
+	readers, err := CreateReaders(ctx, lm)
+	if err != nil {
+		return nil, nil, err
+	}
+	sim_instances, err := CreateSimInstanceFromTopology(ctx, topo, buffer_size)
+	if err != nil {
+		return nil, nil, err
+	}
+	return readers, sim_instances, nil
+}
+
+func InitializeFromFile(ctx context.Context, buffer_size int) (map[string]*parser.Reader, map[string]*SimInstance, error) {
+
+	flag.Parse()
+	if *topology_file == "" {
+		log.Println("topology file for the simulation not provided")
+		log.Fatal("Usage: go run simple_net.go -topology=path/to/topology.json -log=path/to/log_mapping.json")
+	}
+
+	if *logmapping == "" {
+		log.Println("Log mapping file for the simulation not provided")
+		log.Fatal("Usage: go run simple_net.go -topology=path/to/topology.json -log=path/to/log_mapping.json")
+	}
+
+	topo, err := topology.ParseTopology(*topology_file)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	lm, err := topology.ParseLogMapping(*logmapping)
+	if err != nil {
+		return nil, nil, err
+	}
+	return Initialize(ctx, lm, topo, buffer_size)
 }
